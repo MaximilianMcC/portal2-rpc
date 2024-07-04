2c7c3996e1351ff1216f522c19b17ecf48ea05df
MaximilianMcC <max@maximilian.co.nz>
2024-07-04 22:58:05 +1200

    works but very scuffed

---
+++ src/Program.cs
@@ -0,0 +1,80 @@
+﻿using DiscordRPC;
+
+class Program
+{
+	public static DiscordRpcClient Client;
+	public static RichPresence Presence;
+
+	public static void Main(string[] args)
+	{
+		// Give a fancy title thingy
+		Console.Title = "Portal 2 RPC";
+		Console.WriteLine("     Portal 2 Discord Rich Presence");
+		Console.WriteLine("--------------------------------------------");
+
+		// Instructions
+		Console.WriteLine("1. Launch Portal 2");
+		Console.WriteLine("2. Open the developer console");
+		Console.WriteLine("3. Run 'con_logfile console.log'");
+		Console.WriteLine("4. Press any key on this window to start the Rich Presence");
+		Console.ReadKey(true);
+		Console.WriteLine("--------------------------------------------");
+
+		// Start and enable the rich presence 
+		Client = new DiscordRpcClient("1258253632785350787");
+		Client.Initialize();
+
+		// Set the initial presence
+		Presence = new RichPresence()
+		{
+			Assets = new Assets() { LargeImageKey = "aperture_logo_blue" },
+			Timestamps = new Timestamps() { Start = DateTime.UtcNow },
+			Details = "Debugulations rn"
+		};
+		Client.SetPresence(Presence);
+		Console.WriteLine("Rich presence started!");
+
+		// Setup the file watcher so that every time the
+		// config file is edited we can do something
+		// TODO: Don't hardcode the path
+		string logFilePath = @"D:\games\steam\steamapps\common\Portal 2\portal2\";
+		FileSystemWatcher fileWatcher = new FileSystemWatcher(logFilePath, "console.log");
+		fileWatcher.Changed += AttemptToUpdateStatus;
+		fileWatcher.EnableRaisingEvents = true;
+
+		Console.ReadKey();
+		Client.Dispose();
+	}
+
+	private static void AttemptToUpdateStatus(object sender, FileSystemEventArgs e)
+	{
+		// Read the new file contents
+		string[] output;
+		try { output = File.ReadAllLines(e.FullPath); }
+		catch (IOException)
+		{
+			// File is being written to by the game (wait a sec before retrying)
+			Thread.Sleep(100);
+			output = File.ReadAllLines(e.FullPath);
+		}
+
+		// Parse the output to determine where
+		// the user currently is in the game
+		string status = "unknown";
+		foreach (string line in output)
+		{
+			if (line.Contains("Host_NewGame on map")) status = "Loading a chamber";
+			if (line.Contains("==== calling mapspawn.nut")) status = "Playing a chamber";
+			if (line.Contains("from server (Server shutting down)")) status = "In the Main Menu";
+		}
+
+		// Clear the file so that we don't have a
+		// massive log file and so that we keep clean
+		// File.WriteAllText(e.FullPath, "");
+
+		// Update the RPC status
+		Console.WriteLine($"[{DateTime.UtcNow}] Detected new status: {status}");
+		Presence.Details = status;
+		Client.SetPresence(Presence);
+	}
+}
