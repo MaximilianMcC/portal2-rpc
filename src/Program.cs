162c12270d87749297f388db91fbab584988f925
MaximilianMcC <max@maximilian.co.nz>
2024-07-09 11:34:30 +1200

    portal2 directory gotten & autoexec generated

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DiscordRPC;
+using TinyDialogsNet;
 
 class Program
 {
@@ -8,12 +9,14 @@ class Program
 	public static void Main(string[] args)
 	{
 		// Give a fancy title thingy
-		Console.Title = "Portal 2 RPC";
+		Console.Title = "Portal 2 RPC config settings thing";
 
 		// Config stuff
 		ConfigHandler.Initialize();
 
-
+		// Check for if they have setup everything. If not
+		// then guide them through the setup process
+		Setup();
 
 		// Start and enable the rich presence 
 		Client = new DiscordRpcClient("1258253632785350787");
@@ -39,6 +42,65 @@ class Program
 
 		Console.ReadKey();
 		Shutdown(fileWatcher.Path);
+	}
+
+	private static void Setup()
+	{
+		// Keep track of if we've shown the instructions
+		// already so we don't flood the console with them
+		// a million times if they cancel
+		bool shownInstructions = false;
+
+		// Keep on asking them for a location
+		while (ConfigHandler.Config.CompletedSetup == false)
+		{
+			if (shownInstructions == false)
+			{
+				// Say what we're going to do and give some instructions
+				Console.WriteLine("\nThis works by reading console output from");
+				Console.WriteLine("Portal 2 as you play. Please select portal2.exe");
+				Console.WriteLine("so that we know where to read your game status from.\n");
+				shownInstructions = true;
+			}
+
+			// Open a file dialogue so that they can show
+			// what directory portal 2 is stored in
+			FileFilter filter = new FileFilter("Portal 2 game", ["portal2.exe"]);
+			string defaultSteamInstallationDirectory = @"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\";
+			var (cancelled, paths) = TinyDialogs.OpenFileDialog("Select portal 2 executable (portal2.exe)", defaultSteamInstallationDirectory, false, filter);
+
+			// Get the answer
+			string portal2ExePath = "";
+			if (cancelled || paths == null)
+			{
+				Console.WriteLine("You've cancelled the search. Press any key to try again...");
+				Console.ReadKey(true);
+				continue;
+			}
+			using (IEnumerator<string> enumerator = paths.GetEnumerator())
+			{
+				if (enumerator.MoveNext()) portal2ExePath = enumerator.Current;
+			}
+
+			// Save the portal 2 location to the config
+			ConfigHandler.Config.Portal2Directory = Path.Join(Path.GetDirectoryName(portal2ExePath), "portal2");
+			Console.WriteLine();
+			Print("Wrote game path to config.");
+
+			// Create or edit the autoexec file so that we can
+			// automatically run the command to get the console output
+			string autoexecFilePath = Path.Combine(ConfigHandler.Config.Portal2Directory, "cfg", "autoexec.cfg");
+			using (StreamWriter writer = new StreamWriter(autoexecFilePath, true))
+			{
+				writer.WriteLine("\n\n# Save the console output to a file. Used for Discord RPC");
+				writer.WriteLine("con_logfile console.log\n\n");
+			}
+			Print("Wrote autoexec file.");
+
+			// Say that we've completed setup
+			ConfigHandler.Config.CompletedSetup = true;
+			ConfigHandler.Save(ConfigHandler.Config);
+		}
 	}
 
 	private static void AttemptToUpdateStatus(object sender, FileSystemEventArgs e)
